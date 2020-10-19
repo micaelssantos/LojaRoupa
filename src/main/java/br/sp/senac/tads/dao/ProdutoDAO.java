04b75f2f261583c7b4ee03d53e2da691734f5cc4
Micael Santos <>
2020-10-19 14:17:46 -0300

    Incluído driver do MySQL ao POM.xml e importações necessárias nas DAOs

--- src/main/java/br/sp/senac/tads/dao/ProdutoDAO.java
+++ src/main/java/br/sp/senac/tads/dao/ProdutoDAO.java
@@ -1,5 +1,19 @@
 package br.sp.senac.tads.dao;
 
+import br.sp.senac.tads.model.Produto;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 public class ProdutoDAO {
-    
+
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";                                                                 //Driver do MySQL a partir da versão 8.0
+    private static final String LOGIN = "admin";                                                                                     //Nome de Usuário do Bando de Dados
+    private static final String SENHA = "adminadmin";                                                                                //Senha de Acesso ao Banco de Dados
+    private static final String URL = "jdbc:mysql://localhost:3306/loja_roupa?useTimezone=true&serverTimezone=UTC&useSSL=false";     //URL do banco de dados
+    private static Connection CONEXAO;
+
 }
