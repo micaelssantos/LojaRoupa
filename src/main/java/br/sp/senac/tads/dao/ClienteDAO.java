c816121ca4e287e0ed5cace0234fac439bde5e65
dilaz <dilaz@DESKTOP-C06I3RJ>
2020-11-02 19:11:04 -0300

    inserir o DAO

--- src/main/java/br/sp/senac/tads/dao/ClienteDAO.java
+++ src/main/java/br/sp/senac/tads/dao/ClienteDAO.java
@@ -16,4 +16,54 @@ public class ClienteDAO {
     private static final String URL = "jdbc:mysql://localhost:3306/loja_roupa?useTimezone=true&serverTimezone=UTC&useSSL=false";     //URL do banco de dados
     private static Connection CONEXAO;
 
+    private String adicionarCliente;
+    private String consultarCliente;
+    private String alterarCliente;
+    private String removerCliente;
+
+    public ClienteDAO() {
+    }
+
+    public static Connection getCONEXAO() {
+        return CONEXAO;
+    }
+
+    public static void setCONEXAO(Connection CONEXAO) {
+        ClienteDAO.CONEXAO = CONEXAO;
+    }
+
+    public String getAdicionarCliente() {
+        return adicionarCliente;
+    }
+
+    public void setAdicionarCliente(String adicionarCliente) {
+        this.adicionarCliente = adicionarCliente;
+    }
+
+    public String getConsultarCliente() {
+        return consultarCliente;
+    }
+
+    public void setConsultarCliente(String consultarCliente) {
+        this.consultarCliente = consultarCliente;
+    }
+
+    public String getAlterarCliente() {
+        return alterarCliente;
+    }
+
+    public void setAlterarCliente(String alterarCliente) {
+        this.alterarCliente = alterarCliente;
+    }
+
+    public String getRemoverCliente() {
+        return removerCliente;
+    }
+
+    public void setRemoverCliente(String removerCliente) {
+        this.removerCliente = removerCliente;
+    }
+    
+    
+    
 }
