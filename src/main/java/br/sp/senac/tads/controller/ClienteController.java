7f6f1249d87f949e9f62833db9a1a4a98b17315d
dilaz <dilaz@DESKTOP-C06I3RJ>
2020-11-03 22:54:20 -0300

    alterações gerais

--- src/main/java/br/sp/senac/tads/controller/ClienteController.java
+++ src/main/java/br/sp/senac/tads/controller/ClienteController.java
@@ -1,5 +1,79 @@
 package br.sp.senac.tads.controller;
 
+import br.sp.senac.tads.dao.ClienteDAO;
+import br.sp.senac.tads.model.Cliente;
+
 public class ClienteController {
-    
+
+    public static boolean Adicionar(String nome, String CPF, String dataNascimento, String estadoCivil, String sexo, String CEP,
+            String logradouro, String numero, String telefone, String celular, String email) {
+        Cliente obj = new Cliente();
+        obj.setNomeCliente(nome);
+        obj.setCPF(CPF);
+        obj.setDataNascimento(dataNascimento);
+        obj.setEstadoCivil(estadoCivil);
+        obj.setSexo(sexo);
+        obj.setCEP(CEP);
+        obj.setLogradouro(logradouro);
+        obj.setNumero(numero);
+        obj.setTelefone(telefone);
+        obj.setCelular(celular);
+        obj.setEmail(email);
+
+        return ClienteDAO.adicionar(obj);
+    }
+
+    public static boolean Consultar(String nome, String CPF, String dataNascimento, String estadoCivil, String sexo, String CEP,
+            String logradouro, String numero, String telefone, String celular, String email) {
+        Cliente obj = new Cliente();
+        obj.setNomeCliente(nome);
+        obj.setCPF(CPF);
+        obj.setDataNascimento(dataNascimento);
+        obj.setEstadoCivil(estadoCivil);
+        obj.setSexo(sexo);
+        obj.setCEP(CEP);
+        obj.setLogradouro(logradouro);
+        obj.setNumero(numero);
+        obj.setTelefone(telefone);
+        obj.setCelular(celular);
+        obj.setEmail(email);
+
+        return ClienteDAO.consultar(obj);
+    }
+
+    public static boolean alterar(String nome, String CPF, String dataNascimento, String estadoCivil, String sexo, String CEP,
+            String logradouro, String numero, String telefone, String celular, String email) {
+        Cliente obj = new Cliente();
+        obj.setNomeCliente(nome);
+        obj.setCPF(CPF);
+        obj.setDataNascimento(dataNascimento);
+        obj.setEstadoCivil(estadoCivil);
+        obj.setSexo(sexo);
+        obj.setCEP(CEP);
+        obj.setLogradouro(logradouro);
+        obj.setNumero(numero);
+        obj.setTelefone(telefone);
+        obj.setCelular(celular);
+        obj.setEmail(email);
+
+        return ClienteDAO.alterar(obj);
+    }
+
+    public static boolean Remover(String nome, String CPF, String dataNascimento, String estadoCivil, String sexo, String CEP,
+            String logradouro, String numero, String telefone, String celular, String email) {
+        Cliente obj = new Cliente();
+        obj.setNomeCliente(nome);
+        obj.setCPF(CPF);
+        obj.setDataNascimento(dataNascimento);
+        obj.setEstadoCivil(estadoCivil);
+        obj.setSexo(sexo);
+        obj.setCEP(CEP);
+        obj.setLogradouro(logradouro);
+        obj.setNumero(numero);
+        obj.setTelefone(telefone);
+        obj.setCelular(celular);
+        obj.setEmail(email);
+
+        return ClienteDAO.remover(obj);
+    }
 }
--- src/main/java/br/sp/senac/tads/dao/ClienteDAO.java
+++ src/main/java/br/sp/senac/tads/dao/ClienteDAO.java
@@ -1,69 +1,273 @@
 package br.sp.senac.tads.dao;
 
 import br.sp.senac.tads.model.Cliente;
+import br.sp.senac.tads.util.GerenciadorConexao;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 
 public class ClienteDAO {
-    
-    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";                                                                 //Driver do MySQL a partir da versão 8.0
-    private static final String LOGIN = "admin";                                                                                     //Nome de Usuário do Bando de Dados
-    private static final String SENHA = "adminadmin";                                                                                //Senha de Acesso ao Banco de Dados
+
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";            //Driver do MySQL a partir da versão 8.0
+    private static final String LOGIN = "admin";                                           //Nome de Usuário do Bando de Dados
+    private static final String SENHA = "";                                   //Senha de Acesso ao Banco de Dados
     private static final String URL = "jdbc:mysql://localhost:3306/loja_roupa?useTimezone=true&serverTimezone=UTC&useSSL=false";     //URL do banco de dados
     private static Connection CONEXAO;
 
-    private String adicionarCliente;
-    private String consultarCliente;
-    private String alterarCliente;
-    private String removerCliente;
+    public static boolean adicionar(Cliente pCliente) {
+        boolean retorno = false;
+        Connection conexao = null;
+        PreparedStatement instrucaoSQL = null;
 
-    public ClienteDAO() {
+        try {
+
+            //Obs: A classe GerenciadorConexao já carrega o Driver e define os parâmetros de conexão
+            conexao = GerenciadorConexao.abrirConexao();
+
+            instrucaoSQL = conexao.prepareStatement("INSERT INTO cliente (nome,CPF, dataNascimento, estadoCivil,"
+                    + "sexo, CEP, logradouro, numero, telefone, celular, email) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+                     Statement.RETURN_GENERATED_KEYS);  //Caso queira retornar o ID
+
+            //Adiciono os parâmetros ao meu comando SQL
+            instrucaoSQL.setString(1, pCliente.getNomeCliente());
+            instrucaoSQL.setString(2, pCliente.getCPF());
+            instrucaoSQL.setString(3, pCliente.getDataNascimento());
+            instrucaoSQL.setString(4, pCliente.getEstadoCivil());
+            instrucaoSQL.setString(5, pCliente.getSexo());
+            instrucaoSQL.setString(6, pCliente.getCEP());
+            instrucaoSQL.setString(7, pCliente.getLogradouro());
+            instrucaoSQL.setString(8, pCliente.getNumero());
+            instrucaoSQL.setString(9, pCliente.getTelefone());
+            instrucaoSQL.setString(10, pCliente.getCelular());
+            instrucaoSQL.setString(11, pCliente.getEmail());
+
+            int linhasAfetadas = instrucaoSQL.executeUpdate();
+
+            if (linhasAfetadas > 0) {
+                retorno = true;
+
+                ResultSet generatedKeys = instrucaoSQL.getGeneratedKeys(); //Recupero o ID do cliente
+                if (generatedKeys.next()) {
+                    pCliente.setId(generatedKeys.getInt(1));
+                } else {
+                    throw new SQLException("Falha ao cadastrar Cliente!.");
+                }
+            } else {
+                retorno = false;
+            }
+
+        } catch (SQLException | ClassNotFoundException ex) {
+
+            System.out.println(ex.getMessage());
+            retorno = false;
+        } finally {
+
+            //Libero os recursos da memória
+            try {
+                if (instrucaoSQL != null) {
+                    instrucaoSQL.close();
+                }
+
+                GerenciadorConexao.fecharConexao();
+
+            } catch (SQLException ex) {
+            }
+        }
+
+        return retorno;
     }
 
-    public static Connection getCONEXAO() {
-        return CONEXAO;
+    public static boolean consultar(Cliente pCliente) {
+        boolean retorno = false;
+        Connection conexao = null;
+        PreparedStatement instrucaoSQL = null;
+
+        try {
+
+            //Obs: A classe GerenciadorConexao já carrega o Driver e define os parâmetros de conexão
+            conexao = GerenciadorConexao.abrirConexao();
+
+            instrucaoSQL = conexao.prepareStatement("SELECT * FROM cliente (nome,CPF, dataNascimento, estadoCivil,"
+                    + "sexo, CEP, logradouro, numero, telefone, celular, email) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+                     Statement.RETURN_GENERATED_KEYS);  //Caso queira retornar o ID
+
+            //Adiciono os parâmetros ao meu comando SQL
+            instrucaoSQL.setString(1, pCliente.getNomeCliente());
+            instrucaoSQL.setString(2, pCliente.getCPF());
+            instrucaoSQL.setString(3, pCliente.getDataNascimento());
+            instrucaoSQL.setString(4, pCliente.getEstadoCivil());
+            instrucaoSQL.setString(5, pCliente.getSexo());
+            instrucaoSQL.setString(6, pCliente.getCEP());
+            instrucaoSQL.setString(7, pCliente.getLogradouro());
+            instrucaoSQL.setString(8, pCliente.getNumero());
+            instrucaoSQL.setString(9, pCliente.getTelefone());
+            instrucaoSQL.setString(10, pCliente.getCelular());
+            instrucaoSQL.setString(11, pCliente.getEmail());
+
+            int linhasAfetadas = instrucaoSQL.executeUpdate();
+
+            if (linhasAfetadas > 0) {
+                retorno = true;
+
+                ResultSet generatedKeys = instrucaoSQL.getGeneratedKeys(); //Recupero o ID do cliente
+                if (generatedKeys.next()) {
+                    pCliente.setId(generatedKeys.getInt(1));
+                } else {
+                    throw new SQLException("Falha ao obter o ID do Cliente!.");
+                }
+            } else {
+                retorno = false;
+            }
+
+        } catch (SQLException | ClassNotFoundException ex) {
+
+            System.out.println(ex.getMessage());
+            retorno = false;
+        } finally {
+
+            //Libero os recursos da memória
+            try {
+                if (instrucaoSQL != null) {
+                    instrucaoSQL.close();
+                }
+
+                GerenciadorConexao.fecharConexao();
+
+            } catch (SQLException ex) {
+            }
+        }
+
+        return retorno;
     }
 
-    public static void setCONEXAO(Connection CONEXAO) {
-        ClienteDAO.CONEXAO = CONEXAO;
+    public static boolean alterar(Cliente pCliente) {
+        boolean retorno = false;
+        Connection conexao = null;
+        PreparedStatement instrucaoSQL = null;
+
+        try {
+
+            //Obs: A classe GerenciadorConexao já carrega o Driver e define os parâmetros de conexão
+            conexao = GerenciadorConexao.abrirConexao();
+
+            instrucaoSQL = conexao.prepareStatement("ALTER TABLE cliente (nome,CPF, dataNascimento, estadoCivil,"
+                    + "sexo, CEP, logradouro, numero, telefone, celular, email) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+                     Statement.RETURN_GENERATED_KEYS);  //Caso queira retornar o ID
+
+            //Adiciono os parâmetros ao meu comando SQL
+            instrucaoSQL.setString(1, pCliente.getNomeCliente());
+            instrucaoSQL.setString(2, pCliente.getCPF());
+            instrucaoSQL.setString(3, pCliente.getDataNascimento());
+            instrucaoSQL.setString(4, pCliente.getEstadoCivil());
+            instrucaoSQL.setString(5, pCliente.getSexo());
+            instrucaoSQL.setString(6, pCliente.getCEP());
+            instrucaoSQL.setString(7, pCliente.getLogradouro());
+            instrucaoSQL.setString(8, pCliente.getNumero());
+            instrucaoSQL.setString(9, pCliente.getTelefone());
+            instrucaoSQL.setString(10, pCliente.getCelular());
+            instrucaoSQL.setString(11, pCliente.getEmail());
+
+            int linhasAfetadas = instrucaoSQL.executeUpdate();
+
+            if (linhasAfetadas > 0) {
+                retorno = true;
+
+                ResultSet generatedKeys = instrucaoSQL.getGeneratedKeys(); //Recupero o ID do cliente
+                if (generatedKeys.next()) {
+                    pCliente.setId(generatedKeys.getInt(1));
+                } else {
+                    throw new SQLException("Falha ao alterar Cliente!.");
+                }
+            } else {
+                retorno = false;
+            }
+
+        } catch (SQLException | ClassNotFoundException ex) {
+
+            System.out.println(ex.getMessage());
+            retorno = false;
+        } finally {
+
+            //Libero os recursos da memória
+            try {
+                if (instrucaoSQL != null) {
+                    instrucaoSQL.close();
+                }
+
+                GerenciadorConexao.fecharConexao();
+
+            } catch (SQLException ex) {
+            }
+        }
+
+        return retorno;
     }
 
-    public String getAdicionarCliente() {
-        return adicionarCliente;
+    public static boolean remover(Cliente pCliente) {
+        boolean retorno = false;
+        Connection conexao = null;
+        PreparedStatement instrucaoSQL = null;
+
+        try {
+
+            //Obs: A classe GerenciadorConexao já carrega o Driver e define os parâmetros de conexão
+            conexao = GerenciadorConexao.abrirConexao();
+
+            instrucaoSQL = conexao.prepareStatement("DROP TABLE cliente (nome,CPF, dataNascimento, estadoCivil,"
+                    + "sexo, CEP, logradouro, numero, telefone, celular, email) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+                     Statement.RETURN_GENERATED_KEYS);  //Caso queira retornar o ID
+
+            //Adiciono os parâmetros ao meu comando SQL
+            instrucaoSQL.setString(1, pCliente.getNomeCliente());
+            instrucaoSQL.setString(2, pCliente.getCPF());
+            instrucaoSQL.setString(3, pCliente.getDataNascimento());
+            instrucaoSQL.setString(4, pCliente.getEstadoCivil());
+            instrucaoSQL.setString(5, pCliente.getSexo());
+            instrucaoSQL.setString(6, pCliente.getCEP());
+            instrucaoSQL.setString(7, pCliente.getLogradouro());
+            instrucaoSQL.setString(8, pCliente.getNumero());
+            instrucaoSQL.setString(9, pCliente.getTelefone());
+            instrucaoSQL.setString(10, pCliente.getCelular());
+            instrucaoSQL.setString(11, pCliente.getEmail());
+
+            int linhasAfetadas = instrucaoSQL.executeUpdate();
+
+            if (linhasAfetadas > 0) {
+                retorno = true;
+
+                ResultSet generatedKeys = instrucaoSQL.getGeneratedKeys(); //Recupero o ID do cliente
+                if (generatedKeys.next()) {
+                    pCliente.setId(generatedKeys.getInt(1));
+                } else {
+                    throw new SQLException("Falha ao excluir Cliente!.");
+                }
+            } else {
+                retorno = false;
+            }
+
+        } catch (SQLException | ClassNotFoundException ex) {
+
+            System.out.println(ex.getMessage());
+            retorno = false;
+        } finally {
+
+            //Libero os recursos da memória
+            try {
+                if (instrucaoSQL != null) {
+                    instrucaoSQL.close();
+                }
+
+                GerenciadorConexao.fecharConexao();
+
+            } catch (SQLException ex) {
+            }
+        }
+
+        return retorno;
     }
 
-    public void setAdicionarCliente(String adicionarCliente) {
-        this.adicionarCliente = adicionarCliente;
-    }
-
-    public String getConsultarCliente() {
-        return consultarCliente;
-    }
-
-    public void setConsultarCliente(String consultarCliente) {
-        this.consultarCliente = consultarCliente;
-    }
-
-    public String getAlterarCliente() {
-        return alterarCliente;
-    }
-
-    public void setAlterarCliente(String alterarCliente) {
-        this.alterarCliente = alterarCliente;
-    }
-
-    public String getRemoverCliente() {
-        return removerCliente;
-    }
-
-    public void setRemoverCliente(String removerCliente) {
-        this.removerCliente = removerCliente;
-    }
-    
-    
-    
 }
