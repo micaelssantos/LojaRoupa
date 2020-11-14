be32d2c2df7f515b80f099f4934964e506805617
João Pedro <joaopedro.cs12@hotmail.com>
2020-11-14 20:43:52 -0300

    Vendas Feito: DAO, Controller e Model
    Itens
    Feitos: Model

--- src/main/java/br/sp/senac/tads/controller/VendaController.java
+++ src/main/java/br/sp/senac/tads/controller/VendaController.java
@@ -1,5 +1,62 @@
 package br.sp.senac.tads.controller;
 
-public class VendaController {
+import br.sp.senac.tads.dao.VendaDAO;
+import br.sp.senac.tads.model.Venda;
+import java.sql.Date;
+import javax.xml.crypto.Data;
+
+public class VendaController 
+{
     
+  
+    public static boolean Adicionar(float valorVenda, int idcliente, String nomeCliente, Date data) {
+        
+        Venda model = new Venda();
+        
+        model.setNomeCliente(nomeCliente);
+        model.setIdCliente(idcliente);
+        model.setData(data);
+        model.setValorvenda(valorVenda);
+   
+
+        return VendaDAO.Inserir(model);
+    }
+
+    public static boolean Consultar(float valorVenda, int idcliente, String nomeCliente, Date data) {
+        
+        Venda model = new Venda();
+        
+        model.setNomeCliente(nomeCliente);
+        model.setIdCliente(idcliente);
+        model.setData(data);
+        model.setValorvenda(valorVenda);
+   
+
+        return VendaDAO.Consultar(model);
+    }
+     public static boolean Alterar(float valorVenda, int idcliente, String nomeCliente, Date data) {
+        
+        Venda model = new Venda();
+        
+        model.setNomeCliente(nomeCliente);
+        model.setIdCliente(idcliente);
+        model.setData(data);
+        model.setValorvenda(valorVenda);
+   
+
+        return VendaDAO.Alterar(model);
+    }
+
+   public static boolean Remover(float valorVenda, int idcliente, String nomeCliente, Date data) {
+        
+        Venda model = new Venda();
+        
+        model.setNomeCliente(nomeCliente);
+        model.setIdCliente(idcliente);
+        model.setData(data);
+        model.setValorvenda(valorVenda);
+   
+
+        return VendaDAO.Remover(model);
+    }
 }
--- src/main/java/br/sp/senac/tads/dao/ItemVendaDAO.java
+++ src/main/java/br/sp/senac/tads/dao/ItemVendaDAO.java
@@ -7,13 +7,74 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import javax.swing.JOptionPane;
 
 public class ItemVendaDAO {
 
     private static final String DRIVER = "com.mysql.cj.jdbc.Driver";                                                                 //Driver do MySQL a partir da versão 8.0
-    private static final String LOGIN = "admin";                                                                                     //Nome de Usuário do Bando de Dados
-    private static final String SENHA = "adminadmin";                                                                                //Senha de Acesso ao Banco de Dados
+    private static final String LOGIN = "root";                                                                                     //Nome de Usuário do Bando de Dados
+    private static final String SENHA = "1234";                                                                                //Senha de Acesso ao Banco de Dados
     private static final String URL = "jdbc:mysql://localhost:3306/loja_roupa?useTimezone=true&serverTimezone=UTC&useSSL=false";     //URL do banco de dados
-    private static Connection CONEXAO;
+    private static Connection CONEXAO = null;
+    
+    PreparedStatement instrucaoSQL;
+    
+     public static Connection abrirConexao() throws ClassNotFoundException,SQLException {
+        
+        if(CONEXAO==null)      
+        {    
+            try {
+                Class.forName(DRIVER);  
+                CONEXAO = DriverManager.getConnection(URL, LOGIN, SENHA);
+             
+            } catch (ClassNotFoundException e) { 
+                throw new ClassNotFoundException("O driver expecificado nao foi encontrado.");
+
+            } catch (SQLException e) {  
+                throw new SQLException("Erro ao estabelecer a conexão (Ex: login ou senha errados).");
+            }
+        }
+        else
+        {
+            try {
+                
+                if(CONEXAO.isClosed())
+                    CONEXAO = DriverManager.getConnection(URL, LOGIN, SENHA);
+            } catch (SQLException ex) {
+                throw new SQLException("Falha ao fechar a conexão.");
+            }
+        }
+        return CONEXAO;
+    }
+     
+     
+     
+    public void Inserir(br.sp.senac.tads.model.ItemVenda itemModel)
+    {
+        try 
+      {
+            abrirConexao();
+            
+            instrucaoSQL = CONEXAO.prepareStatement("INSERT INTO ITEM_VENDA (ID_PRODUTO, ID_VENDA, NOME_PRODUTO, QUANTIDADE, VALOR_ITEM) VALUES(?, ?, ?, ?, ?)");
+            instrucaoSQL.setInt(1,itemModel.getIdProduto());
+            instrucaoSQL.setInt(2,itemModel.getIdVenda());
+            instrucaoSQL.setString(3,itemModel.getNomeProduto());
+            instrucaoSQL.setInt(4,itemModel.getQtd());
+            instrucaoSQL.setFloat(5,itemModel.getVlUnit());
+            
+            int linhaAfetadas = instrucaoSQL.executeUpdate();
+
+             if(linhaAfetadas>0)
+            {
+               JOptionPane.showMessageDialog(null,"Informações cadastradas com sucesso!");
+            }           
+       }   
+      catch (ClassNotFoundException ex) {
+                JOptionPane.showMessageDialog(null, "Erro ao carregar o driver!");
+            } catch (SQLException ex) {
+                JOptionPane.showMessageDialog(null, "Erro ao abrir a conexão!");
+            }
+    
+    }
 
 }
