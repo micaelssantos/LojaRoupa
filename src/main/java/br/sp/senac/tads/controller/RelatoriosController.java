1097df1bd79584bfb46723dde97b74f64676fa6b
Micael Santos <micael.sousa.fenex@gmail.com>
2020-11-30 16:29:30 -0300

    Adicionado comentários para JavaDOC

--- src/main/java/br/sp/senac/tads/controller/RelatoriosController.java
+++ src/main/java/br/sp/senac/tads/controller/RelatoriosController.java
@@ -6,19 +6,22 @@ import java.sql.Date;
 import java.util.ArrayList;
 
 /**
- *@author renea
+ * @author renea
  * @see br.sp.senac.tads.dao.RelatoriosDAO
  * @see br.sp.senac.tads.model.Relatorios
  */
 public class RelatoriosController {
-  
- /**
- * @param - Objeto do Relatório Sintético
- * @return ArrayList - Apresenta as vendas realizadas num determinda período
- * Método que apresenta a consulta das vendas por período
- */
+
     RelatoriosDAO relatorio = new RelatoriosDAO();
 
+    /**
+     * Método responsável por listar o relatório Sintético.
+     *
+     * Objeto do Relatório Sintético
+     *
+     * @return ArrayList - Apresenta as vendas realizadas num determinda período
+     * Método que apresenta a consulta das vendas por período
+     */
     public ArrayList listarRelatorioController(String inicio, String fim) {
 
         ArrayList<Relatorios> listaVendas = new ArrayList<Relatorios>();
@@ -28,11 +31,14 @@ public class RelatoriosController {
         return listaVendas;
     }
 
- /**
- * @param - Objeto do Relatório Análitico
- * @return ArrayList - Apresenta a venda detalhada, item a item, de uma venda realizada em um determinado cliente
- * Método de consultar da venda especifica de seu cliente
- */
+    /**
+     * Metódo responsável por listas o relatório Analítico.
+     *
+     * @param id Objeto do Relatório Análitico
+     * @return ArrayList - Apresenta a venda detalhada, item a item, de uma
+     * venda realizada em um determinado cliente Método de consultar da venda
+     * especifica de seu cliente
+     */
     public ArrayList listarRelatorioAnaliticoController(int id) {
 
         ArrayList<Relatorios> listaVendas = new ArrayList<Relatorios>();
